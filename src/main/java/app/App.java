8aa322b8d565b54858e134babf3a22e5213e639f
Khrir <karllisson.15henrique@hotmail.com>
2022-09-17 16:24:54 -0300

    add static files

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,12 +1,16 @@
 package app;
 
+import java.nio.file.Paths;
 import io.jooby.Jooby;
 
 public class App extends Jooby {
 
   {
 
-    get("/", ctx -> "Welcome to Jooby!");
+    // get("/", ctx -> "Welcome to Jooby!");
+    assets("/assets/*", Paths.get("assets"));
+    get("/", ctx -> Paths.get("assets/index.html"));
+    get("/card", ctx -> Paths.get("assets/card.html"));
   }
 
   public static void main(final String[] args) {
