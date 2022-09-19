9f2cad53f9b8b126d099b23f0090bbd8b5a8c616
Khrir <karllisson.15henrique@hotmail.com>
2022-09-19 20:04:34 -0300

    Iniciação bd

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,37 +2,45 @@ package app;
 
 import java.nio.file.Paths;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
 import org.jooby.jdbc.Jdbc;
 
 import app.Controllers.PhraseController;
+import app.dao.PhraseDao;
 import app.model.PhraseModel;
 import io.jooby.Jooby;
 
 public class App extends Jooby {
 
   {
-    // // install(new MyTemplateEngineModule());
+    mount(new Jdbc("db"));
     assets("/assets/*", Paths.get("assets"));
-    // get("/", ctx -> Paths.get("assets/index.html"));
     
-    
-    // get("/my-api", req -> {DataSource ds = require(DataSource.class);}); 
+    // accessing to the data source
+    // get("/bd", req -> {
+    //   DataSource db = require(DataSource.class);
+    //   // do something with datasource
+    // }); 
+
     mvc(new PhraseController());
 
-    // post( "/", req -> {
-    //   PhraseModel phraseModel = req.form(PhraseModel.class);
-    //   System.out.println(phraseModel.getId());
-    //   System.out.println(phraseModel.getTitle());
-    //   System.out.println(phraseModel.getPhrase());
-    //   return true;
-    // });
+    post( "/", req -> {
+      PhraseModel phraseModel = req.form(PhraseModel.class);
+      // phraseDao.inserir(phraseModel);
+      System.out.println(phraseModel.getId());
+      System.out.println(phraseModel.getTitle());
+      System.out.println(phraseModel.getPhrase());
+      return true;
+    });
 
   }
 
   public static void main(final String[] args) {
     runApp(args, App::new);
+  }
+
+  private void mount(Jdbc jdbc) {
   }
 
 }
