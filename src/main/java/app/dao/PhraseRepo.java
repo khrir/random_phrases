2935cc28d885bb859a7d7e86176c99025a162fec
Khrir <karllisson.15henrique@hotmail.com>
2022-09-20 19:26:31 -0300

    .

--- src/main/java/app/dao/PhraseRepo.java
+++ src/main/java/app/dao/PhraseRepo.java
@@ -11,54 +11,24 @@ import app.model.PhraseModel;
 import java.util.List;
 
 /**
- * Database access for pets.
+ * Database access for phrases.
  */
 @RegisterRowMapper(PhraseModel.Mapper.class)
 public interface PhraseRepo {
-  /**
-   * List pets using start/max limits.
-   *
-   * @param start Start offset.
-   * @param max Max number of rows.
-   * @return Available pets.
-   */
+
   @SqlQuery("select * from phrases limit :max offset :start")
   List<PhraseModel> list(int start, int max);
 
-  /**
-   * Find a pet by ID.
-   *
-   * @param id Pet ID.
-   * @return Pet or null.
-   */
   @SqlQuery("select * from phrases where id=:id")
-  PhraseModel findById(long id);
+  PhraseModel findById(int id);
 
-  /**
-   * Insert a pet and returns the generated PK.
-   *
-   * @param pet Pet to insert.
-   * @return Primary key.
-   */
   @SqlUpdate("insert into phrases(title, phrase) values(:title, :phrase)")
   @GetGeneratedKeys
-  long insert(@BindBean PhraseModel pet);
+  long insert(@BindBean PhraseModel phrase);
 
-  /**
-   * Update a pet and returns true if the pets was updated.
-   *
-   * @param pet Pet to update.
-   * @return True if the pet was updated.
-   */
   @SqlUpdate("update phrases set title=:title,phrase=:phrase where id=:id")
-  boolean update(@BindBean PhraseModel pet);
+  boolean update(@BindBean PhraseModel phrase);
 
-  /**
-   * Delete a pet by ID.
-   *
-   * @param id Pet ID.
-   * @return True if the pet was deleted.
-   */
   @SqlUpdate("delete phrases where id=:id")
-  boolean delete(long id);
+  boolean delete(int id);
 }
